194c46fb132befd451a480c79c2d1564cbe569a8
Alaws0n <andrewclawson01@gmail.com>
2023-03-26 18:37:09 +0000

    Checkpoint: Needs Testing

--- scripts/sphere_fit.py
+++ scripts/sphere_fit.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 import rospy
+import math
 import numpy as np
 from robot_vision_lectures.msg import XYZarray
 from robot_vision_lectures.msg import SphereParams
 
 matrix_a = []
 matrix_b = []
+P = np.array([])
+Sphere_Parameters = SphereParams()
 
-# Maybe need a model fitting function
-# Maybe need a function to receive the image like in lab4
+
 def receive_point_data(point_data):
 	global matrix_a
 	global matrix_b
@@ -20,23 +22,48 @@ def receive_point_data(point_data):
 			matrix_b.append([point.x**2 + point.y**2 + point.z**2])
 		except:
 			print("Data is not being appended")
-		
 
+		
+def model_fitting_formula(matrix_a, matrix_b):
+	global P
+	# Create numpy arrays out of our initial matrices
+	A = np.array(matrix_a)
+	B = np.array(matrix_b)
+	# Calculate P
+	try:
+		ATA = np.matmul(A.T, A)
+		ATB = np.matmul(A.T, B)
+		P = np.matmul(np.linalg.inv(ATA), ATB)
+	except:
+		print("Dimension error in calculation! -- Moving on")
+	
+	
+def calculate_sphere_params(P):
+	# Set center parameters for sphere
+	Sphere_Parameters.xc = P[0]
+	Sphere_Parameters.yc = P[1]
+	Sphere_Parameters.zc = P[2]
+	# Calculate radius
+	radius = math.sqrt(P[3] + P[0]**2 + P[1]**2 + P[2]**2)
+	# Set radius parameter
+	Sphere_Parameters.radius = radius
 
 if __name__ == '__main__':
 	# Initialize the ball detection node
 	rospy.init_node('sphere_fit', anonymous = True)
 	# Subscribe to point data
 	point_data_sub = rospy.Subscriber("/xyz_cropped_ball", XYZarray, receive_point_data)
-	# Publish my stuff -- Rename publisher later
-	new_pub = rospy.Publisher("/sphere_params", SphereParams, queue_size = 1)
-	
+	# Publish Sphere Parameters
+	sphere_parameter_pub = rospy.Publisher("/sphere_params", SphereParams, queue_size = 10)
+	# Set 10hz loop rate
 	rate = rospy.Rate(10)
 	
 	while not rospy.is_shutdown():
 		# If our initial matrices are constructed and not empty
 		if len(matrix_a) > 0 and len(matrix_b) > 0:
-			print(matrix_b)
-		
-		# Do some  logic
+			model_fitting_formula(matrix_a, matrix_b)
+			if len(P) > 0:
+				calculate_sphere_params(P)
+				sphere_parameter_pub.publish(Sphere_Parameters)
+			
 		rate.sleep()
