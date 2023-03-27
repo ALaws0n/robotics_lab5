35a1069ab3cfbd9917d1826bef3c98f158207c54
Alaws0n <andrewclawson01@gmail.com>
2023-03-27 01:13:25 +0000

    Finalized Comments

--- scripts/sphere_fit.py
+++ scripts/sphere_fit.py
@@ -5,9 +5,12 @@ import numpy as np
 from robot_vision_lectures.msg import XYZarray
 from robot_vision_lectures.msg import SphereParams
 
+# Create Initial A and B matrices
 matrix_a = []
 matrix_b = []
+# Create empty numpy array for P
 P = np.array([])
+# Create Empty Sphere Parameter Message
 Sphere_Parameters = SphereParams()
 
 
@@ -15,12 +18,13 @@ def receive_point_data(point_data):
 	global matrix_a
 	global matrix_b
 	
-	# Point Data is an array of points
+	# Loop through point data and create our initial A and B matrices
 	for point in point_data.points:
 		try:
 			matrix_a.append([2*point.x, 2*point.y, 2*point.z, 1])
 			matrix_b.append([point.x**2 + point.y**2 + point.z**2])
 		except:
+			# Message to display if something happens when appending data
 			print("Data is not being appended")
 
 		
@@ -29,13 +33,13 @@ def model_fitting_formula(matrix_a, matrix_b):
 	# Create numpy arrays out of our initial matrices
 	A = np.array(matrix_a)
 	B = np.array(matrix_b)
-	# Calculate P
+	# Calculate P -- Catch error when matrice dimensions do not match requirements
 	try:
 		ATA = np.matmul(A.T, A)
 		ATB = np.matmul(A.T, B)
 		P = np.matmul(np.linalg.inv(ATA), ATB)
 	except:
-		print("Dimension error in calculation! -- Moving on")
+		print("Dimension error in calculation -- Continuing on!")
 	
 	
 def calculate_sphere_params(P):
@@ -53,17 +57,19 @@ if __name__ == '__main__':
 	rospy.init_node('sphere_fit', anonymous = True)
 	# Subscribe to point data
 	point_data_sub = rospy.Subscriber("/xyz_cropped_ball", XYZarray, receive_point_data)
-	# Publish Sphere Parameters
+	# Create Publisher for Sphere Parameters
 	sphere_parameter_pub = rospy.Publisher("/sphere_params", SphereParams, queue_size = 10)
 	# Set 10hz loop rate
 	rate = rospy.Rate(10)
 	
 	while not rospy.is_shutdown():
-		# If our initial matrices are constructed and not empty
+		# If our initial matrices are constructed and not empty -- Run model fitting function
 		if len(matrix_a) > 0 and len(matrix_b) > 0:
 			model_fitting_formula(matrix_a, matrix_b)
+			# If P has received valid data -- Calculate Sphere Parameters
 			if len(P) > 0:
 				calculate_sphere_params(P)
+				# Publish Sphere Parameters
 				sphere_parameter_pub.publish(Sphere_Parameters)
 			
 		rate.sleep()
